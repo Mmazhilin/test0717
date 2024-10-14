e94440c209aee7ff19c21f2126495b4fe76d468d
Mmazhilin <45139687+Mmazhilin@users.noreply.github.com>
2024-10-14 15:43:10 +0800

    Delete src/main directory

--- src/main/webapp/js/mapUrl.js
+++
@@ -1,9 +0,0 @@
-var jsonMap =
-{
-	"width":"100%",
-	"height":"100%",
-	"mapUrl": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14478.633450596004!2d120.992902!3d24.8755153!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346831596468c83d%3A0x5da7a96f57c9053f!2zQW8gcGFzdGHnvqnlpKfliKnpurU!5e0!3m2!1szh-TW!2stw!4v1716169527372!5m2!1szh-TW!2stw",
-	"styleBorder":"border: 0",
-	"loading":"lazy",
-	"no_referrer":"no-referrer-when-downgrade"
-};
