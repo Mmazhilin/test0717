25aaca4ea32e2d5f0fc8581eec1f9042163c89b8
Mmazhilin <45139687+Mmazhilin@users.noreply.github.com>
2024-10-23 03:23:14 +0800

    Delete test0717 directory

--- test0717/src/main/webapp/js/mapUrl.js
+++
@@ -1,9 +0,0 @@
-var jsonMap =
-{
-	"width":"100%",
-	"height":"100%",
-	"mapUrl": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3619.839960832649!2d120.996645!3d24.869315!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346831505ce20595%3A0x1d13efe96c43c466!2z5paw6LGQ6LuK56uZ!5e0!3m2!1szh-TW!2stw!4v1728891820554!5m2!1szh-TW!2stw",
-	"styleBorder":"border: 0",
-	"loading":"lazy",
-	"no_referrer":"no-referrer-when-downgrade"
-};
